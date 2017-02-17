1b8c2276cf0e5ff68a34c1cd9322ea62dbce3590
Zenon 'SI <si.zenon@gmail.com>
2017-02-17 10:00:30 +0700

    Add a garden 572115016 Chetthaphan Kammasit

--- se323-activity1/src/edu/camt323/act1/Boss.java
+++
@@ -1,7 +0,0 @@
-package edu.camt323.act1;
-
-/**
- * Created by sizen on 17/2/2560.
- */
-public class Boss {
-}
---
+++ se323-activity1/src/edu/camt323/act1/model/impl/Garden.java
@@ -0,0 +1,7 @@
+package edu.camt323.act1.model.impl;
+
+import edu.camt323.act1.model.Plantable;
+
+public class Garden {
+    private Plantable gardenArea[][];
+}

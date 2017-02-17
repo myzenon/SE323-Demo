895f7a69dd5dee1a7454043d6f3f0773d22a2220
shadowangle <shadowangle1222@hotmail.com>
2017-02-17 10:15:42 +0700

    Natthagid Thonnachot 572115018

---
+++ se323-activity1/src/edu/camt323/act1/model/impl/tree.java
@@ -0,0 +1,17 @@
+package edu.camt323.act1.model.impl;
+
+import edu.camt323.act1.model.Growable;
+
+public class tree implements Growable {
+	
+	private String type;
+	private String name;
+	
+	
+
+	@Override
+	public void grow() {
+		
+		
+	}
+}

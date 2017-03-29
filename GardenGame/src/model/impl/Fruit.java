4cfdd4d85d47a10368624c1527b3902aba435f63
jetsirawut <onpraphai.sira@gmail.com>
2017-03-29 14:48:56 +0700

    Add Fruit / Jet

---
+++ GardenGame/src/model/impl/Fruit.java
@@ -0,0 +1,21 @@
+package model.impl;
+
+public class Fruit {
+	private String name;
+	private int size;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+
+}

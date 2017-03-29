580f7cf02338dd237c62009889c00165e7c9fe1b
jetsirawut <onpraphai.sira@gmail.com>
2017-03-29 15:25:48 +0700

    Add Plants and iGrowable

---
+++ GardenGame/src/model/Growable.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Growable {
+public void grow();
+}
---
+++ GardenGame/src/model/impl/Plant.java
@@ -0,0 +1,22 @@
+package model.impl;
+
+import model.Growable;
+
+public class Plant implements Growable {
+private int health; // main health
+private int max_water; // max - armor
+private int water; // armor - watering maxes water
+private Fruit fruit;
+
+
+public enum age {Seed, Seedling, Mature, Dead}
+	
+	@Override
+	public void grow() {
+		// TODO Auto-generated method stub
+	}
+	public void produce(){
+		
+	}
+
+}
